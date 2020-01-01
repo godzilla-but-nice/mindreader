5df60eb2079a7952bb99aef52ca5cf06f959bc10
godzilla-but-nice <patrick.gordon.wall@gmail.com>
2019-12-31 22:44:08 -0500

    abspath

--- bot.py
+++ bot.py
@@ -12,8 +12,8 @@ THRESHOLD = 0.81
 MIN_CHARS = 15
 EMOTIONS = {0: 'neg', 2: 'neu', 1: 'pos'}  # TODO: make sure keys are correct
 TRAINING_DATA = 'training_data/smaller_tweets.csv'
-PICKLE_VECT = 'pickle_objects/vectorizer.pkl'
-PICKLE_CLF = 'pickle_objects/classifier.pkl'
+PICKLE_VECT = os.path.abspath('pickle_objects/vectorizer.pkl')
+PICKLE_CLF = os.path.abspath('pickle_objects/classifier.pkl')
 
 
 async def init_server_db(server, servers):  # TODO: enforce schema
